6afa823402b26c3de5314ac232bd4552bd2b937d
pnicpon <37027916+pnicpon@users.noreply.github.com>
2018-03-04 00:04:18 +0100

    Initial commit

---
+++ MetaKeywords/Main.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="MetaKeywords.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Słowa kluczowe</h1>
+        </div>
+        <asp:Panel ID="pnlUrl" runat="server" DefaultButton="">
+            <asp:Label ID="lblUrl" runat="server" Text="Wprowadź URL"></asp:Label>
+            <asp:TextBox ID="txtUrl" runat="server" autocomplete="off"></asp:TextBox>
+            <asp:CheckBox ID="cbShowPage" runat="server" Text="Wyświetl stronę" />
+            <asp:Button ID="btnClick" runat="server" Text="Sprawdź słowa kluczowe" OnClick="btnClick_Click" ValidationGroup="form" />
+            <br />            
+            <asp:RequiredFieldValidator ID="rfvUrl" runat="server" ControlToValidate="txtUrl" Display="Dynamic" ForeColor="Red" ErrorMessage="Wprowadź adres url" ValidationGroup="form"></asp:RequiredFieldValidator>
+        </asp:Panel>
+        <asp:Panel ID="pnlMessage" runat="server" Visible="false">
+            <asp:Literal ID="ltrMessage" runat="server"></asp:Literal>
+        </asp:Panel>
+        <asp:Panel ID="pnlResult" runat="server" Visible="false" style="float:left; width:250px; margin-top:10px;">
+            <asp:Literal ID="ltrInfo" runat="server"></asp:Literal>
+        </asp:Panel>
+        <asp:Panel ID="pnlResult2" runat="server" Visible="false" style="float:left; width:250px; margin-top:10px;">
+            <asp:Literal ID="ltrInfo2" runat="server"></asp:Literal>
+        </asp:Panel>
+        <asp:Panel ID="pnlPreview" runat="server" Visible="false">
+            <iframe id="frame" runat="server" scrolling="auto" style="width:100%; height: 500px;"></iframe>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ MetaKeywords/Main.aspx.cs
@@ -0,0 +1,171 @@
+﻿using HtmlAgilityPack;
+using MSHTML;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MetaKeywords
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void btnClick_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid == false)
+            {
+                pnlMessage.Visible = true;
+                ltrMessage.Text = "Wprowadź adres url";
+
+                return;
+            }
+
+            try
+            {
+                string url = txtUrl.Text.Trim();
+
+                if (url.StartsWith("http://") == false && url.StartsWith("https://") == false)
+                    url = "http://" + url;
+
+                pnlPreview.Visible = pnlMessage.Visible = pnlResult.Visible = pnlResult2.Visible = false;
+
+                CheckByMSHTML(url);
+                CheckByHtmlAgilityPack(url);
+                
+                if (cbShowPage.Checked)
+                {
+                    frame.Src = url;
+                    pnlPreview.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                pnlMessage.Visible = true;
+                pnlResult.Visible = pnlResult2.Visible = pnlPreview.Visible = false;
+
+                ltrMessage.Text = "Nie udało się pobrać zawartości strony.";
+            }
+        }
+        
+        #region Private Methods
+
+        private void CheckByMSHTML(string url)
+        {
+            // Get page from url
+            WebRequest request = WebRequest.Create(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            
+            string responseFromServer = reader.ReadToEnd();
+            
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            
+            IHTMLDocument2 doc = new HTMLDocumentClass();
+            doc.write(new object[] { responseFromServer });
+            doc.close();
+
+            // Get keywords list
+            List<string> keywordsList = new List<string>();
+            foreach (IHTMLElement el in (IHTMLElementCollection)doc.all)
+            {
+                if (el.tagName == "META")
+                {
+                    HTMLMetaElement meta = (HTMLMetaElement)el;
+                    if (string.IsNullOrEmpty(meta.name) || meta.name.ToLower() != "keywords" || string.IsNullOrEmpty(meta.content))
+                        continue;
+
+                    keywordsList.AddRange(meta.content.Split(',').ToList());
+                }
+            }
+            
+            string innerText = doc.body.innerText.ToLower();
+            string result = "<b>Użycie MSHTML</b><br/>";
+
+            if (innerText != null && keywordsList.Count != 0)
+            {
+                foreach (string key in keywordsList)
+                {
+                    int count = 0;
+                    int i = 0;
+
+                    while ((i = innerText.IndexOf(key.ToLower(), i)) != -1)
+                    {
+                        i += key.Length;
+                        count++;
+                    }
+
+                    //foreach (Match match in Regex.Matches(bodyHtml, key, RegexOptions.IgnoreCase))
+                    //{
+                    //    count++;
+                    //}
+
+                    result += "<br/>" + key + ": " + count;
+                }
+            }
+            else
+                result += "<br/> - nie zwróciło wyników";
+
+            pnlResult.Visible = true;
+            ltrInfo.Text = result;
+        }
+
+        private void CheckByHtmlAgilityPack(string url)
+        {
+            // HtmlAgilityPack
+            List<string> keywordsList = new List<string>();
+
+            var webGet = new HtmlWeb();
+            var document = webGet.Load(url);
+            var metaTags = document.DocumentNode.SelectNodes("//meta");
+            if (metaTags != null)
+            {
+                foreach (var tag in metaTags)
+                {
+                    var tagName = tag.Attributes["name"];
+                    var tagContent = tag.Attributes["content"];
+                    var tagProperty = tag.Attributes["property"];
+                    if (tagName != null && tagContent != null && tagName.Value.ToLower() == "keywords")
+                    {
+                        keywordsList.AddRange(tagContent.Value.Split(',').ToList());
+                    }
+                }
+            }
+
+            string innerText = document.DocumentNode.SelectSingleNode("//body").InnerText.ToLower();
+            string result = "<b>Użycie HtmlAgilityPack</b><br/>";
+
+            if (string.IsNullOrWhiteSpace(innerText) == false && keywordsList.Count != 0)
+            {
+                foreach (string key in keywordsList)
+                {
+                    if (string.IsNullOrWhiteSpace(key))
+                        continue;
+
+                    int count = 0;
+                    foreach (Match match in Regex.Matches(innerText, key.Trim(), RegexOptions.IgnoreCase))
+                    {
+                        count++;
+                    }
+
+                    result += "<br/>" + key.Trim() + ": " + count;
+                }
+            }
+            else
+                result += "<br/> - nie zwróciło wyników";
+
+            pnlResult2.Visible = true;
+            ltrInfo2.Text = result;
+        }
+
+        #endregion Private Methods
+    }
+}
